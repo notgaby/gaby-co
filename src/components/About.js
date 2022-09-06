08f76b4d7080f90a47e524585810f60035533c7e
Gabrielle Co <gabriellejco@example.com>
2022-09-06 14:40:46 -0500

    cleanup

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import selfie from '../imgs/me.png';
 import pdf from '../imgs/Gabrielle_Co_Resume.pdf';
-import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faFontAwesome, faGithub, faEtsy } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faEtsy } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import Typewriter from 'typewriter-effect';
 
-//&nbsp;
 const ShowEmoji = (props) => (
      <span
         className="emoji"
@@ -15,13 +13,50 @@ const ShowEmoji = (props) => (
         aria-label={props.label ? props.label : ""}
         aria-hidden={props.label ? "false" : "true"}
     >
-        
         {props.symbol}
     </span>
-  )
+)
+
+const Bio = () => (
+    <body>
+        Im an upcoming December 2022 graduate from the University of Houston Honors College with a major in Computer Science
+        with minors in math and data within society!
+        <br/> <br/>
+        I have interned with Liberty Mutual Insurance and NerdWallet in the past alongside prep and insight programs
+        from IBM, Meta, Citibank, Blackrock, CodePath, and RewritingtheCode.
+        <br/>
+        <span className='highlight'>
+        <ShowEmoji symbol="✨"/>
+        I am currently looking for full-time software engineering opportunities 
+        starting 2023<ShowEmoji symbol="✨"/></span>
+        <br/> <br/>
+        <p>
+            Outside of University, I spend time exploring coffee shops <ShowEmoji symbol="☕️"/>, 
+            designing for my Etsy shop <ShowEmoji symbol="🎨"/>, and making flan <ShowEmoji symbol="🍮"/> :)
+        </p>
+    </body>
+)
+
+const TypewriterText = () => (
+    <Typewriter 
+    options={{autoStart: true, loop: true}}
+    onInit={(typewriter) => {
+        typewriter.typeString("Howdy, \n\nI'm Gaby!")
+        .start()
+        .pauseFor(2000);       
+    }}/>
+)
+
+const Icons = () => (
+    <div className='icons'>
+        <a href='https://www.linkedin.com/in/gabrielleco/'><FontAwesomeIcon style={{padding: '2px', fontSize: '40px'}} icon= {faLinkedin} /></a>
+        <a href='https://github.com/notgaby'><FontAwesomeIcon style={{padding: '2px', fontSize: '40px'}} icon= {faGithub} /></a>
+        <a href='https://www.etsy.com/shop/gabcodesigns'><FontAwesomeIcon style={{padding: '2px', fontSize: '40px'}} icon= {faEtsy} /></a>
+        <a href='mailto:gabriellejco@gmail.com'><FontAwesomeIcon style={{padding: '2px', fontSize: '40px'}}icon= {faEnvelope} /></a> 
+    </div>
+)
   
 const About = () => {
-    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
     return(
         <div className='wrapper'>
             <div className='buttons'>
@@ -32,56 +67,21 @@ const About = () => {
 
             <div className='aboutContainer'>
                 <div className='leftAbout'>
-                    <div className='aboutItem' style={{color: 'red'}}>
+                    <div className='aboutItem'>
                         <img id='responsive' title="me" src={selfie} alt="drawing of me" />
                     </div>
                 </div>
 
                 <div className='rightAbout'>
-                    <div className='aboutItem' style={{color: 'green'}}>
-
+                    <div className='aboutItem'>
                         <h1 className='hello'>  
-                        <Typewriter 
-                        options={{autoStart: true, loop: true}}
-                        onInit={(typewriter) => {
-                            typewriter.typeString("Howdy, \n\nI'm Gaby!")
-                            .start()
-                            .pauseFor(2000);       
-                        }}/>
-                            </h1>
-                            <h1>
-                            <div className='icons'>
-
-                                
-
-                                <a href='https://www.linkedin.com/in/gabrielleco/'><FontAwesomeIcon style={{padding: '2px', fontSize: '40px'}} icon= {faLinkedin} /></a>
-                                <a href='https://github.com/notgaby'><FontAwesomeIcon style={{padding: '2px', fontSize: '40px'}} icon= {faGithub} /></a>
-                                <a href='https://www.etsy.com/shop/gabcodesigns'><FontAwesomeIcon style={{padding: '2px', fontSize: '40px'}} icon= {faEtsy} /></a>
-                                <a href='mailto:gabriellejco@gmail.com'><FontAwesomeIcon style={{padding: '2px', fontSize: '40px'}}icon= {faEnvelope} /></a> 
-                            </div>
-                            
+                            <TypewriterText className= 'hello'/>
                         </h1>
-
-                        </div>
-
-                        <body >
-                            Im an upcoming December 2022 graduate from the University of Houston with a major in Computer Science
-                            with minors in math and data and society!
-                            <br/> <br/>
-                            I have interned with Liberty Mutual Insurance and NerdWallet in the past alongside prep and insight programs
-                            from IBM, Meta, Citibank, Blackrock, CodePath, and RewritingtheCode.
-                            <br/>
-                            <span className='highlight'>
-                            <ShowEmoji symbol="✨"/>
-                            I am currently looking for full-time software engineering opportunities 
-                            starting 2023<ShowEmoji symbol="✨"/></span>
-                            <br/> <br/>
-                            <p>
-                                Outside of University, I spend time exploring coffee shops <ShowEmoji symbol="🍮"/>, 
-                                designing for my Etsy shop <ShowEmoji symbol="🎨"/>, and making flan <ShowEmoji symbol="🍮"/> :)
-                            </p>
-                        </body>
-
+                        <h1>
+                            <Icons />
+                        </h1>
+                    </div>
+                    <Bio />
                 </div>
             </div>
         </div>
